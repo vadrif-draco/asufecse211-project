2d7bb9f8920c2c49231405a2b815cd3c2b7e1a63
Ahmed1Bakry <52543767+Ahmed1Bakry@users.noreply.github.com>
2021-11-10 12:14:22 +0200

    Added delay and inverted buttons

--- main.c
+++ main.c
@@ -36,12 +36,40 @@ void lab4A_task1() {
 
     while (1) {
 
-        if (!read_pin(PORTF, 0)) switch0_held = 0;
-        if (!read_pin(PORTF, 4)) switch4_held = 0;
-
-        write_pin(PORTF, 3, read_pin(PORTF, 0) && read_pin(PORTF, 4) ? HIGH : LOW);
-        if (read_pin(PORTF, 0) && !switch0_held) { tgl_bit(GPIO_PORTF_DATA_R, 1); switch0_held = 1; }
-        if (read_pin(PORTF, 4) && !switch4_held) { tgl_bit(GPIO_PORTF_DATA_R, 2); switch4_held = 1; }
+        if (read_pin(PORTF, 0) == OFF) 
+        {
+          DELAY(100000);
+          if (read_pin(PORTF, 0) == OFF) 
+          {
+            switch0_held = 0;
+          }
+        }
+        if (read_pin(PORTF, 4) == OFF) 
+        {
+          DELAY(100000);
+          if (read_pin(PORTF, 4) == OFF) 
+          {
+            switch4_held = 0;
+          }
+        }
+        // Green Stuff
+        write_pin(PORTF, 3, read_pin(PORTF, 0) == ON && read_pin(PORTF, 4) == ON ? HIGH : LOW);
+        
+        if (read_pin(PORTF, 0) == ON && !switch0_held) {
+          DELAY(100000);
+          if(read_pin(PORTF, 0) == ON && !switch0_held)
+          {
+            tgl_bit(GPIO_PORTF_DATA_R, 1); 
+            switch0_held = 1; 
+          }
+        }
+        if (read_pin(PORTF, 4) == ON && !switch4_held) {
+          DELAY(100000);
+          if (read_pin(PORTF, 4) == ON && !switch4_held) {
+            tgl_bit(GPIO_PORTF_DATA_R, 2); 
+            switch4_held = 1;
+          }
+        }
 
     }
 
@@ -55,11 +83,37 @@ void lab4A_task2() {
 
     while (1) {
 
-        if (!read_pin(PORTF, 0)) switch0_held = 0;
-        if (!read_pin(PORTF, 4)) switch4_held = 0;
+        if (!read_pin(PORTF, 0)) 
+        {
+          DELAY(10000);
+          if (!read_pin(PORTF, 0)) 
+          {
+            switch0_held = 0;
+          }
+        }
+        if (!read_pin(PORTF, 4)) 
+        {
+          DELAY(10000);
+          if (!read_pin(PORTF, 4)) 
+          {
+            switch4_held = 0;
+          }
+        }
+        if (read_pin(PORTF, 0) && !switch0_held) { 
+          DELAY(10000);
+          if (read_pin(PORTF, 0) && !switch0_held) { 
+            led_state = (led_state + 1) % 4;
+            switch0_held = 1; 
+          }
 
-        if (read_pin(PORTF, 0) && !switch0_held) { led_state = (led_state + 1) % 4; switch0_held = 1; }
-        if (read_pin(PORTF, 4) && !switch4_held) { led_state = (led_state + 4 - 1) % 4; switch4_held = 1; }
+        }
+        if (read_pin(PORTF, 4) && !switch4_held) { 
+          DELAY(10000);
+          if (read_pin(PORTF, 4) && !switch4_held) { 
+            led_state = (led_state + 4 - 1) % 4;
+            switch4_held = 1; 
+          }
+        }
 
         switch (led_state) {
 
@@ -76,14 +130,13 @@ void lab4A_task2() {
 }
 
 int main() {
-
     for (int pin = 1; pin <= 3; pin++) DIO_init(PORTF, pin, OUT); // LED pins
     DIO_init(PORTF, 0, IN); DIO_init(PORTF, 4, IN); // Switch pins
-    GPIO_PORTF_PUR_R = 0x11; // HACK: PDR should be used, but it doesn't work
+    GPIO_PORTF_PUR_R |= 0x11; // HACK: PDR should be used, but it doesn't work
 
     // lab3B_task2();
     // lab3R_task1();
-    // lab4A_task1();
+     // lab4A_task1();
     lab4A_task2();
 
     return 0;
@@ -93,29 +146,19 @@ int main() {
 
 // Old manual code w/o abstraction layers - Lab 3A_task1 + 3A_task2
 // int main() {
-
+//
 //     SYSCTL_RCGCGPIO_R |= 0x20; // Activating the clock on Port F
 //     while ((SYSCTL_PRGPIO_R & 0x20) == 0); // Ensure activation
 //     GPIO_PORTF_LOCK_R = 0x4C4F434B; // Unlock port F registers
 //     GPIO_PORTF_CR_R = 0x1F; // Allow changes in first 5 pins
 //     GPIO_PORTF_DIR_R = 0xE; // Set direction of LED pins out
-//     GPIO_PORTF_PUR_R = 0x11; // Setting switches as pull-up
+//     GPIO_PORTF_PDR_R = 0x11; // Setting switches as pull-up
 //     GPIO_PORTF_DEN_R = 0x1F; // Enabling digital operations
-
+//
 //     while (1) { // System infinite loop
-
-//         for (int j = 0; j < 8; j++) { // Looping through the eight states of RGB
-
-//             for (volatile long long int i = 0; i < 256000; i++); // With delay
-//             GPIO_PORTF_DATA_R += 0x2;
-
-//         }
-//         // Then, after a longer delay, turn all the lights off
-//         for (volatile long long int i = 0; i < 512000; i++);
-//         GPIO_PORTF_DATA_R &= !(0xE);
-
+//        write_pin(PORTF, 1, !read_pin(PORTF, 0));
 //     }
-
+//
 //     return 0;
-
+//
 // }
